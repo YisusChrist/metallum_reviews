f55d711e4ce5ef330c1c13d0562aa61156d73791
YisusChrist <agelrenorenardo@gmail.com>
2024-10-20 19:01:56 +0200

    refactor: replace existing print_welcome with core_helpers functionality

--- metallum_reviews/__main__.py
+++ metallum_reviews/__main__.py
@@ -11,6 +11,7 @@ from requests_cache import CachedSession
 from rich import print
 from rich.prompt import Prompt
 from rich.traceback import install
+from core_helpers.utils import print_welcome
 
 from metallum_reviews.consts import GITHUB, PACKAGE
 from metallum_reviews.consts import __desc__ as DESC
@@ -19,62 +20,10 @@ from metallum_reviews.models import Review
 from metallum_reviews.utils import fetch_reviews, reviews_to_json
 
 
-def strip_rich_tags(text: str) -> str:
-    """
-    Remove rich text tags from a string.
-
-    Args:
-        text (str): The text to remove tags from.
-
-    Returns:
-        str: The text with tags removed.
-    """
-    return re.sub(r"\[\/?[\w=#]*\]", "", text)
-
-
-def get_random_font() -> str:
-    """
-    Return a random font from pyfiglet.
-
-    Returns:
-        str: A random font name.
-    """
-    fonts: list[str] = pyfiglet.FigletFont.getFonts()
-    return random.choice(fonts)
-
-
-def print_welcome() -> None:
-    """Print a welcome message in the terminal."""
-    # Get terminal width
-    width: int = os.get_terminal_size().columns
-
-    # Pickup a random font
-    font: str = "slant"  # get_random_font()
-
-    # Create and format title, repository, and description
-    title: str = PACKAGE.replace("_", " ").capitalize()
-    version: str = f"[i][red]Version: {VERSION}[/]"
-    repo: str = f"[cyan]{GITHUB}[/]"
-    desc: str = f"[blue]{DESC}[/] - {version}"
-
-    # Render and print title using pyfiglet
-    figlet = pyfiglet.Figlet(font=font, justify="center", width=width)
-    print(f"""[green]{figlet.renderText(title)}[/]""")
-
-    # Calculate visible lengths and center accordingly
-    visible_desc: str = strip_rich_tags(desc)
-    visible_repo: str = strip_rich_tags(repo)
-
-    # Print description and repository info with rich formatting
-    print(desc.center(width + len(desc) - len(visible_desc)))
-    print(repo.center(width + len(repo) - len(visible_repo)))
-    print()
-
-
 def main() -> None:
     """Main function."""
     install()
-    print_welcome()
+    print_welcome(PACKAGE, VERSION, DESC, GITHUB)
 
     try:
         band: str = Prompt.ask("Enter the band to search", default="Opeth")
